891210836d537e8396d45409d88799b5d37f6dbd
wasw100 <wasw100@gmail.com>
2014-08-08 22:28:32 +0800

    update CrimeActivity and CrimeFragment

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.criminalintent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,9 +25,16 @@ import android.view.ViewGroup;
 public class CrimeActivity extends FragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if(fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+		}
 	}
 
 
--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,9 +2,12 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * CrimeFragment类是与模型及视图对象交互的控制器，用于显示特定crime的明细信息，并在用户修改这些信息后立即进行内容更新。
@@ -14,6 +17,7 @@ import android.view.ViewGroup;
 public class CrimeFragment extends Fragment {
 
 	private  Crime mCrime;
+	private EditText mTitleField;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,25 @@ public class CrimeFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence c, int start, int before, int count) {
+				mCrime.setTitle(c.toString());
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+			}
+			
+			@Override
+			public void afterTextChanged(Editable arg0) {
+			}
+		});
+		
+		
 		return v;
 	}
 }
