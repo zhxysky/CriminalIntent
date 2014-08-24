c1b0cab50ebb1b2ea09bb3b45b06d99fe59334b1
zhxysky <zhxysky@qq.com>
2014-08-24 17:09:19 +0800

    show dialog when click date button in detail page

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -28,6 +29,8 @@ public class CrimeFragment extends Fragment {
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
+	
+	private static final String DIALOG_DATE = "date";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -70,7 +73,18 @@ public class CrimeFragment extends Fragment {
 		//引用新添加的按钮
 		mDateButton = (Button) v.findViewById(R.id.crime_date);
 		mDateButton.setText((new SimpleDateFormat("MM/dd/yy h:mmaa")).format(mCrime.getDate()));
-		mDateButton.setEnabled(false);
+//		mDateButton.setEnabled(false);
+		//设置点击日期按钮弹出对话框
+		mDateButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				DatePickerFragment dialog = new DatePickerFragment();
+				dialog.show(fm, DIALOG_DATE);
+				
+			}
+		});
 		
 		
 		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
